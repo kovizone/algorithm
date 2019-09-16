1034f5de8332b2697a864f18a253985c74bf30be
Kovi <kovichen@163.com>
2019-09-16 10:53:39 +0800

    update algorithm

---
+++ src/main/java/com/kovizone/algorithm/Algorithm_002_IntegerAddition.java
@@ -0,0 +1,26 @@
+package com.kovizone.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 不使用加运算符进行整形计算
+ */
+public class Algorithm_002_IntegerAddition {
+
+    @Test
+    public void test() {
+        int a = 3;
+        int b = -6;
+
+        int sum = add(a, b);
+
+        System.out.println(String.format("    a = %d (%s)", a, Integer.toBinaryString(a)));
+        System.out.println(String.format("    b = %d (%s)", b, Integer.toBinaryString(b)));
+        System.out.println(String.format("a + b = %d (%s)", sum, Integer.toBinaryString(sum)));
+    }
+
+    public int add(int a, int b) {
+        return b == 0 ? a : add(a ^ b, (a & b) << 1);
+    }
+
+}
