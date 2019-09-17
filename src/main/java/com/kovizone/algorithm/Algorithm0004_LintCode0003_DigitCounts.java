37cface9462acc75b2fb36fc22a0a2b25b3b226a
Kovi <kovichen@163.com>
2019-09-17 11:19:35 +0800

    update algorithm

--- src/main/java/com/kovizone/algorithm/Algorithm0004_LintCode0003_DigitCounts.java
+++ src/main/java/com/kovizone/algorithm/Algorithm0004_LintCode0003_DigitCounts.java
@@ -13,20 +13,25 @@ public class Algorithm0004_LintCode0003_DigitCounts {
     @Test
     public void test() {
 
-        long start = 0L;
         int k = 2;
         int n = 15615758;
+
+        long start = 0L;
+
         System.out.println(String.format("k = %d, n = %d", k, n));
         start = System.currentTimeMillis();
         int a = digitCounts(k, n);
-        System.out.println(a);
+        System.out.println("digitCounts result: " + a);
         System.out.println("用时：" + (System.currentTimeMillis() - start));
+
         start = System.currentTimeMillis();
         long b = stupidMethod(k, n);
-        System.out.println(b);
+        System.out.println("stupidMethod result: " + b);
         System.out.println("用时：" + (System.currentTimeMillis() - start));
         if (a == b) {
             System.out.println("测试通过");
+        } else {
+            System.out.println("测试不通过");
         }
     }
 
@@ -40,42 +45,61 @@ public class Algorithm0004_LintCode0003_DigitCounts {
             }
         }
 
+        // 统计结果
+        int count = 0;
+
         // 长度
         int length = String.valueOf(n).length();
 
-        // 游标
-        int startNum = 0;
-        int count = 0;
-
-        int flag = 1;
-        while (flag < length - 1) {
-
-            // x - 涨幅
-            int x = 1;
-            for (int i = 0; i < length - flag; i++) {
-                x *= 10;
-            }
-
-            // defaultDetailCount - 每阶段默认增幅
-            int defaultDetailCount = x / 10 * (length - flag);
-
-            for (; startNum + x - 1 <= n; startNum += x) {
-
-                String startNumStr = String.valueOf(startNum);
-                char[] startNumChars = startNumStr.toCharArray();
-
-                int detailCount = defaultDetailCount;
-                for (int i = 0; i < flag; i++) {
-                    if (Integer.parseInt(String.valueOf(startNumChars[i])) == k) {
-                        detailCount += x;
-                    }
+        // 计算n的长度减一位数的最大值对k的统计
+        // 如：若n是四位数，根据规律获得0~999的k统计
+        int lengthFlag = length - 1;
+        if (lengthFlag == 1) {
+            count++;
+        } else if (lengthFlag == 2) {
+            count += (k == 0 ? 10 : 20);
+        } else {
+            if (k == 0) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(lengthFlag - 2);
+                for (int i = 0; i < lengthFlag - 3; i++) {
+                    sb.append(8);
                 }
-                count += detailCount;
+                sb.append(90);
+                count += Integer.parseInt(sb.toString());
+            } else {
+                count = (int) Math.pow(10, (lengthFlag - 1)) * lengthFlag;
             }
-            flag++;
         }
 
-        for (long i = startNum; i <= n; i++) {
+        // 游标，以为已经获取了n-1位数最大值的统计数，所以游标从n的长度位最小数开始
+        // 如n是四位数，则游标从1000开始，这是4位数中的最小数
+        int vernier = (int) Math.pow(10, lengthFlag);
+
+        while (lengthFlag > 1) {
+
+            // vernierNext - 游标每次递进的值
+            // 刚好该值是规律中统计的增值
+            int vernierNext = (int) Math.pow(10, lengthFlag);
+
+            // defaultCount - 每阶段默认增幅
+            int defaultCount = (int) Math.pow(10, (lengthFlag - 1)) * lengthFlag;
+
+            while (vernier + vernierNext - 1 <= n) {
+                char[] startNumChars = String.valueOf(vernier).toCharArray();
+                for (int i = 0; i < length - lengthFlag; i++) {
+                    if (Integer.parseInt(String.valueOf(startNumChars[i])) == k) {
+                        count += vernierNext;
+                    }
+                }
+                count += defaultCount;
+                vernier += vernierNext;
+            }
+            lengthFlag--;
+        }
+
+        // 计算剩余数
+        for (long i = vernier; i <= n; i++) {
             char[] chars = String.valueOf(i).toCharArray();
             for (char c : chars) {
                 if (Integer.parseInt(String.valueOf(c)) == k) {
